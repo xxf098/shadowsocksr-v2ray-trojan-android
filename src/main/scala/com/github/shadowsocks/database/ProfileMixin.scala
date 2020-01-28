317a2471d3cf3912b8fccfe699d08989ee15698f
xxf098 <ufxmeng@gmail.com>
2020-01-27 23:40:46 -0500

    testConnection

--- src/main/scala/com/github/shadowsocks/database/ProfileMixin.scala
+++ src/main/scala/com/github/shadowsocks/database/ProfileMixin.scala
@@ -1,12 +1,19 @@
 package com.github.shadowsocks.database
 
-import java.io.IOException
+import java.io.{File, IOException}
+import java.lang.System.currentTimeMillis
 import java.nio.charset.StandardCharsets
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 import android.text.TextUtils
+import com.github.shadowsocks.{GuardedProcess, R}
 import com.github.shadowsocks.ShadowsocksApplication.app
-import com.github.shadowsocks.utils.Utils
+import com.github.shadowsocks.utils.{ConfigUtils, ExeNative, NetUtils, TcpFastOpen, Utils}
+import okhttp3.{OkHttpClient, Request}
 import tun2socks.Tun2socks
+
+import scala.collection.mutable.ArrayBuffer
 
 
 trait ProfileAction {
@@ -16,7 +23,43 @@ trait ProfileAction {
 }
 
 object SSRAction extends ProfileAction {
-  override def getElapsed(): Long = throw new Exception("Not Supported!")
+  // TODO: refactor
+  var ssTestProcess: GuardedProcess = _
+  override def getElapsed(): Long = {
+    var elapsed = 0L
+    try {
+      var host = profile.host
+      if (!Utils.isNumeric(host)) Utils.resolve(host, enableIPv6 = true) match {
+        case Some(addr) => host = addr
+        case None => throw new Exception("can't resolve")
+      }
+      val conf = ConfigUtils
+        .SHADOWSOCKS.formatLocal(Locale.ENGLISH, host, profile.remotePort, profile.localPort + 2,
+        ConfigUtils.EscapedJson(profile.password), profile.method, 600, profile.protocol, profile.obfs, ConfigUtils.EscapedJson(profile.obfs_param), ConfigUtils.EscapedJson(profile.protocol_param))
+      Utils.printToFile(new File(app.getApplicationInfo.dataDir + "/ss-local-test.conf"))(p => {
+        p.println(conf)
+      })
+      val cmd = ArrayBuffer[String](Utils.getAbsPath(ExeNative.SS_LOCAL)
+        , "-t", "600"
+        , "-L", "www.google.com:80"
+        , "-c", app.getApplicationInfo.dataDir + "/ss-local-test.conf")
+
+      if (TcpFastOpen.sendEnabled) cmd += "--fast-open"
+      ssTestProcess = new GuardedProcess(cmd).start()
+      val start = currentTimeMillis
+      while (start - currentTimeMillis < 3 * 1000 && NetUtils.isPortAvailable(profile.localPort + 2)) {
+        try {
+          Thread.sleep(100)
+        } catch {
+          case e: InterruptedException => Unit
+        }
+      }
+      elapsed = NetUtils.testConnection("http://127.0.0.1:" + (profile.localPort + 2) + "/generate_204")
+    } finally {
+      Option(ssTestProcess).foreach(_.destroy())
+    }
+    elapsed
+  }
 
   override def isOK(): Boolean = !(TextUtils.isEmpty(profile.host) || TextUtils.isEmpty(profile.password))
 }
---
+++ src/main/scala/com/github/shadowsocks/utils/NetUtils.scala
@@ -0,0 +1,56 @@
+package com.github.shadowsocks.utils
+
+import java.lang.System.currentTimeMillis
+import java.net.Socket
+import java.util.concurrent.TimeUnit
+
+import com.github.shadowsocks.R
+import com.github.shadowsocks.ShadowsocksApplication.app
+import com.github.shadowsocks.database.SSRAction.profile
+import okhttp3.{OkHttpClient, Request}
+
+import scala.util.{Success, Try}
+
+object NetUtils {
+
+  private val TAG = "NetUtils"
+
+  def isPortAvailable(port: Int): Boolean = {
+    // Assume no connection is possible.
+    var result = true
+    try {
+      new Socket("127.0.0.1", port).close()
+      result = false;
+    } catch {
+      case e: Exception => Unit
+    }
+    result
+  }
+
+  def testConnection(url: String): Long = {
+    var elapsed = 0L
+    val builder = new OkHttpClient.Builder()
+      .connectTimeout(5, TimeUnit.SECONDS)
+      .writeTimeout(5, TimeUnit.SECONDS)
+      .readTimeout(5, TimeUnit.SECONDS)
+    val client = builder.build()
+    val request = new Request.Builder()
+      .url(url).removeHeader("Host").addHeader("Host", "www.google.com")
+      .build()
+    val response = client.newCall(request).execute()
+    val code = response.code()
+    if (code == 204 || code == 200 && response.body().contentLength == 0) {
+      val start = currentTimeMillis
+      val response = client.newCall(request).execute()
+      elapsed = currentTimeMillis - start
+      val code = response.code()
+      if (code == 204 || code == 200 && response.body().contentLength == 0) {
+        response.body().close()
+      }
+      else throw new Exception(app.getString(R.string.connection_test_error_status_code, code: Integer))
+    } else throw new Exception(app.getString(R.string.connection_test_error_status_code, code: Integer))
+    response.body().close()
+    elapsed
+  }
+
+}

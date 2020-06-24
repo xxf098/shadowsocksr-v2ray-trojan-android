4978f30954a2e0cb33d3e6f80e933d1f68a3246c
xxf098 <ufxmeng@gmail.com>
2020-06-24 03:41:03 -0400

    Testing profiles

--- src/main/scala/com/github/shadowsocks/utils/Constants.scala
+++ src/main/scala/com/github/shadowsocks/utils/Constants.scala
@@ -373,6 +373,9 @@ object Key {
   val AUTO_UPDATE_SUBSCRIPTION = "pref_auto_update_subscription"
   val AUTO_TEST_CONNECTIVITY = "pref_auto_test_connectivity"
   val SSR_DNS_NOCAHCE = "pref_ssr_dns_nocache1"
+
+  // message
+  val TEST_PROFILE_IDS = "test_profile_ids"
 }
 
 object State {

49949ed54756ac5011d48008f5fac0027a1b1989
xxf098 <ufxmeng@gmail.com>
2021-07-28 21:15:27 -0400

    update latency

--- src/main/java/tun2socks/TestDownload.java
+++ src/main/java/tun2socks/TestDownload.java
@@ -2,7 +2,7 @@ package tun2socks;
 
 public interface TestDownload
 {
-    void updateSpeed(final long p0, final long p1);
+    void updateSpeed(final long p0, final long p1, final long p2);
 
     void updateTraffic(final long p0, final long p1);
 }
